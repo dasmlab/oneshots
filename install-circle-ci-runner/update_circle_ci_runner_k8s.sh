35585cb3a77c2668e75a50d733f732a35f186797
Daniel Smith <lmcdasm@gmail.com>
2025-06-19 14:45:16 +0000

    cleaning up after merge

--- install-circle-ci-runner/update_circle_ci_runner_k8s.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run this to update/upgrade your existing deployment (like after you change the values.yaml with new params)
-helm upgrade  container-agent container-agent/container-agent -n circleci -f values.yaml
-
